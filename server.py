25c3ccb31fc5515afa1e766a5ea4eee111f8b60b
AuthurightCode <74897259+authuright@users.noreply.github.com>
2024-01-22 03:37:41 +0000

    setup flask

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello Flask"
+
+@app.route('/main')
+def main():
+    return "Dashboard Flask"
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0",debug=True)
